8e2c0e70b2713eb3c1fc9b7dc2396cbf25e6f8b4
Vidyasagar Yadav <vidyasagaryadav499@gmail.com>
2025-01-06 05:20:10 -0800

    Add Lombok integration

--- src/main/java/com/example/chronos/entity/JournalEntry.java
+++ src/main/java/com/example/chronos/entity/JournalEntry.java
@@ -1,49 +1,20 @@
 package com.example.chronos.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
+@Data
 @Document(collection = "journal_entry")
 public class JournalEntry {
 
-    @Id // Primary key.
-    private ObjectId id;
+    @Id
+    private ObjectId id; // Primary key.
     private LocalDateTime date;
     private String title;
     private String content;
 
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 }
